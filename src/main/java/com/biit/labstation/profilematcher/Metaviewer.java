136ea16a65fdc3e6d39525fc8b614a6e7dd35e89
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-30 16:03:51 +0200

    Fixing test

--- src/main/java/com/biit/labstation/profilematcher/Metaviewer.java
+++ src/main/java/com/biit/labstation/profilematcher/Metaviewer.java
@@ -1,7 +1,9 @@
 package com.biit.labstation.profilematcher;
 
 import com.biit.labstation.CustomChromeDriver;
+import com.biit.labstation.ToolTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -26,19 +28,25 @@ public class Metaviewer {
     public void openElement(int bulletIndex) {
         customChromeDriver.findElementWaiting(By.id("metaviewer-board")).findElements(By.className("metaviewer-element"))
                 .get(bulletIndex).click();
+        ToolTest.waitComponent();
     }
 
     public String getMetaviewerElementHeader() {
-        return customChromeDriver.findElementWaiting(By.id("metaviewer-data")).findElement(By.id("metaviewer-header"))
-                .findElement(By.id("title")).getText();
+        return customChromeDriver.findElementWaiting(By.id("metadata-viewer")).findElement(By.id("metaviewer-data"))
+                .findElement(By.id("metaviewer-header")).findElement(By.id("title")).getText();
     }
 
     public String getMetaviewerElementData(int index) {
-        return customChromeDriver.findElementWaiting(By.id("metaviewer-data")).findElement(By.id("metaviewer-info"))
-                .findElements(By.id("metadata-info-.row")).get(index).findElement(By.id("field")).findElement(By.id("input")).getText();
+        return customChromeDriver.findElementWaiting(By.id("metadata-viewer")).findElement(By.id("metaviewer-data")).findElement(By.id("metaviewer-info"))
+                .findElements(By.id("metaviewer-info-row")).get(index).findElement(By.id("field")).findElement(By.id("input")).getText();
     }
 
     public void metaviewerElementClose() {
-        customChromeDriver.findElementWaiting(By.id("metaviewer-data")).findElement(By.id("close-button")).click();
+        customChromeDriver.findElementWaiting(By.id("metadata-viewer")).findElement(By.id("metaviewer-data")).findElement(By.id("close-button")).click();
+    }
+
+    public WebElement getMetaviewerFilter(int index) {
+        return customChromeDriver.findElementWaiting(By.id("metadata-filter")).findElement(By.id("metaviewer-data")).findElement(By.id("metaviewer-info"))
+                .findElements(By.id("metaviewer-info-row")).get(index).findElement(By.id("field")).findElement(By.id("input"));
     }
 }
