0696c2e02f6fa384081861662bc83f197fcc2947
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-09-10 17:20:32 +0200

    Added activity and historic tests

--- src/main/java/com/biit/labstation/components/Table.java
+++ src/main/java/com/biit/labstation/components/Table.java
@@ -114,7 +114,7 @@ public class Table {
 
     public void selectRow(TableId tableId, String label, int column) {
         search(tableId, label);
-        await().atMost(Duration.ofSeconds(WAITING_TIME_SECONDS*100)).until(() -> {
+        await().atMost(Duration.ofSeconds(WAITING_TIME_SECONDS)).until(() -> {
             for (int i = 0; i < countRows(tableId); i++) {
                 if (Objects.equals(getText(tableId, i, column), label)) {
                     selectRow(tableId, i);
